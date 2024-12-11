b78bb002eea0167273cb9aae6cb58cdf5a38e4a9
Luuk <luuk.machielse@gmail.com>
2024-12-11 16:01:25 +0100

    Refactored arrayToCsv.go to be more clear and consise

--- pkg/data/arraysToCsv.go
+++ pkg/data/arraysToCsv.go
@@ -8,51 +8,55 @@ import (
 	"github.com/CodingCat12/pacgraph/pkg/config"
 )
 
+type Attribute struct {
+	Field func(pkg Package) []string
+	File  string
+	Name  string
+}
+
 func ConvertArrays(packages []Package) error {
-	type attributeData struct {
-		field   func(pkg Package) []string
-		file    string
-		name    string
-		records [][]string
+
+	attributes := []Attribute{
+		{func(pkg Package) []string { return pkg.Groups }, config.AdjustedConfig.Paths.GroupsFile, "groups"},
+		{func(pkg Package) []string { return pkg.Licenses }, config.AdjustedConfig.Paths.LicensesFile, "licenses"},
+		{func(pkg Package) []string { return pkg.Conflicts }, config.AdjustedConfig.Paths.ConflictsFile, "conflicts"},
+		{func(pkg Package) []string { return pkg.Provides }, config.AdjustedConfig.Paths.ProvidesFile, "provides"},
+		{func(pkg Package) []string { return pkg.Replaces }, config.AdjustedConfig.Paths.ReplacesFile, "replaces"},
+		{func(pkg Package) []string { return pkg.Depends }, config.AdjustedConfig.Paths.DependsFile, "depends"},
+		{func(pkg Package) []string { return pkg.Optdepends }, config.AdjustedConfig.Paths.OptDependsFile, "optdepends"},
+		{func(pkg Package) []string { return pkg.Makedepends }, config.AdjustedConfig.Paths.MakeDependsFile, "makedepends"},
+		{func(pkg Package) []string { return pkg.Checkdepends }, config.AdjustedConfig.Paths.CheckDependsFile, "checkdepends"},
 	}
 
-	attributes := []attributeData{
-		{func(pkg Package) []string { return pkg.Groups }, config.AdjustedConfig.Paths.GroupsFile, "groups", nil},
-		{func(pkg Package) []string { return pkg.Licenses }, config.AdjustedConfig.Paths.LicensesFile, "licenses", nil},
-		{func(pkg Package) []string { return pkg.Conflicts }, config.AdjustedConfig.Paths.ConflictsFile, "conflicts", nil},
-		{func(pkg Package) []string { return pkg.Provides }, config.AdjustedConfig.Paths.ProvidesFile, "provides", nil},
-		{func(pkg Package) []string { return pkg.Replaces }, config.AdjustedConfig.Paths.ReplacesFile, "replaces", nil},
-		{func(pkg Package) []string { return pkg.Depends }, config.AdjustedConfig.Paths.DependsFile, "depends", nil},
-		{func(pkg Package) []string { return pkg.Optdepends }, config.AdjustedConfig.Paths.OptDependsFile, "optdepends", nil},
-		{func(pkg Package) []string { return pkg.Makedepends }, config.AdjustedConfig.Paths.MakeDependsFile, "makedepends", nil},
-		{func(pkg Package) []string { return pkg.Checkdepends }, config.AdjustedConfig.Paths.CheckDependsFile, "checkdepends", nil},
-	}
+	for _, attr := range attributes {
+		file := attr.File
+		header := []string{"pkg", attr.Name}
+		var records [][]string
 
-	for i := range attributes {
-		file := attributes[i].file
-		header := []string{"pkg", attributes[i].name}
 		err := writeHeader(header, file)
 		if err != nil {
 			return err
 		}
 
-		for j, pkg := range packages {
-			for _, value := range attributes[i].field(pkg) {
-				attributes[i].records = append(attributes[i].records, []string{pkg.Pkgname, value})
+		for i, pkg := range packages {
+			for _, value := range attr.Field(pkg) {
+				records = append(records, []string{pkg.Pkgname, value})
 			}
 
-			if ((j + 1) % config.AdjustedConfig.BatchSize) == 0 {
-				err := writeToCsv(attributes[i].records, file)
+			if ((i + 1) % config.AdjustedConfig.BatchSize) == 0 {
+				err := writeToCsv(records, file)
 				if err != nil {
 					return err
 				}
-				attributes[i].records = nil
+				records = nil
 			}
 		}
 
-		err = writeToCsv(attributes[i].records, file)
-		if err != nil {
-			return err
+		if len(records) > 0 {
+			err = writeToCsv(records, file)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
